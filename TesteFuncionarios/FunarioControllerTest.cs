748ff0222ffe10a7e9fa3348631556fadcfba553
Gabriel <gabrielaugustofm@gmail.com>
2023-03-18 10:55:37 -0300

    Adicionado Teste do Metodo Patch

--- TesteFuncionarios/FunarioControllerTest.cs
+++ TesteFuncionarios/FunarioControllerTest.cs
@@ -6,14 +6,40 @@ namespace TesteFuncionarios
     public class FunarioControllerTest
     {
         [Fact]
+        public void GetTest()
+        {
+            limparControler();
+            var controller = criarFuncionarioController();
+            var funcionario = criarFuncionario();
+            addFuncionario(funcionario);
+            var _funcionario = controller.GetFuncionarios().First();
+            Assert.Equal(funcionario, _funcionario);
+        }
+        [Fact]
         public void PostTest()
         {
             limparControler();
             var controller = criarFuncionarioController();
             var funcionario = criarFuncionario();
-            FuncionarioController.Funcionarios.Add(funcionario);
-            var _funcionario = controller.GetFuncionarios().First();
-            Assert.Equal(funcionario, _funcionario);
+            controller.PostFuncionarios(funcionario);
+            var funcionarioInterno = getFuncionario();
+            Assert.Equal(funcionario, funcionarioInterno);
+            Assert.NotEqual(default(Guid),funcionario.id);
+        }
+        [Fact]
+        public void PatchTest()
+        {
+            limparControler();
+            var controller = criarFuncionarioController();
+            var funcionario = criarFuncionario();
+            var funcionarioEditado = criarFuncionario();
+            addFuncionario(funcionario);
+            var id = Guid.NewGuid();
+            funcionario.id = id;
+            funcionarioEditado.id = id;
+            funcionarioEditado.Nome = "Carla";
+            controller.PatchFuncionarios(funcionarioEditado);
+            Assert.Equal(funcionario.Nome, funcionarioEditado.Nome);
         }
         private Funcionario criarFuncionario()
         {
@@ -35,5 +61,16 @@ namespace TesteFuncionarios
         {
             FuncionarioController.Funcionarios = new();
         }
+        private void addFuncionario(Funcionario funcionario)
+        {
+            FuncionarioController.Funcionarios.Add(funcionario);
+
+        }
+        private Funcionario getFuncionario()
+        {
+            return FuncionarioController.Funcionarios.First();
+
+        }
+
     }
 }
